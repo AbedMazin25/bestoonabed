704f3a8482fb3a9255785be885140eed52615ec7
Abed Matinpour <abedmatinpour@yahoo.com>
2018-07-25 07:33:05 -0700

    first income model created

--- web/admin.py
+++ web/admin.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from .models import Expense
+from .models import Income
 from django.contrib import admin
 
 # Register your models here.
 
 admin.site.register(Expense)
-
+admin.site.register(Income)
